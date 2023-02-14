87f4b27ba0084db67ce5cb57c76d8ea7061c83a7
Jasiri M. Wa-Kyendo <jasiri.w@gmail.com>
2023-02-14 23:55:05 +0000

    Fixxed Question A: 4 Anwers

--- L03/Midterm Practice Questions/QuestionA.c
+++ L03/Midterm Practice Questions/QuestionA.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
-bool perfect_square(int);
-
-int main(void){
-    for(int i = 2;i <= 85;i++){
-        if(perfect_square(i+100) && perfect_square(i+268)){
-            printf("%i\n", i);
+int main(){
+    int i;
+    for (i = -100; i < 10000; i++){
+        double a, b;
+        a = sqrt(i+100);
+        b =  sqrt(i+268);
+        if (fabs(a - (int)a) < 0.0001 && fabs(b - (int)b) < 0.0001) {
+            printf("%d\n", i);          
         }
     }
-}
-
-bool perfect_square(int num){
-    for(int i = 0; i < num; i++)
-    {
-        if(i*i == num){
-            return true;
-        }
-    }
-    return false;
+    
 }
