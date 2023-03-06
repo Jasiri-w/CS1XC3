f9864a8055f410b2cf4c00618c603fc796a02e6e
wakyendj <wakyendj@pascal.cas.mcmaster.ca>
2023-03-06 16:01:02 -0500

    L06 Parts 1 & 2

---
+++ L06/.bash_logout
@@ -0,0 +1,6 @@
+# ~/.bash_logout
+
+cd ~/CS1XC3
+git add .
+git commit -m "Shell Logout Commit"
+git push
---
+++ L06/profile.sh
@@ -0,0 +1,29 @@
+
+
+if [ $UID -gt 199 ] && [ "`/usr/bin/id -gn`" = "`/usr/bin/id -un`" ]; then
+    umask 002
+else
+    umask 022
+fi
+
+for i in /etc/profile.d/*.sh /etc/profile.d/sh.local ; do
+    if [ -r "$i" ]; then
+        if [ "${-#*i}" != "$-" ]; then
+            . "$i"
+        else
+            . "$i" >/dev/null
+        fi
+    fi
+done
+
+unset i
+unset -f pathmunge
+
+if [ -n "${BASH_VERSION-}" ] ; then
+        if [ -f /etc/bashrc ] ; then
+                # Bash login shells run only /etc/profile
+                # Bash non-login shells run only /etc/bashrc
+                # Check for double sourcing is done in /etc/bashrc.
+                . /etc/bashrc
+       fi
+fi
